3ac4e432073e692b2cfe282b0f3c06b01b0a317a
FatmanaK <janalikbolsin@gmail.com>
2024-08-29 14:04:34 -0500

    chech console messeges!

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 alert(" Siteye hosgeldiniz!! ");
 
 console.log("hosgeldiniz !!!!") /* chrome da inspect edince console da yazi gormek icin  */
+console.log(123)
+console.error(" Hata olustu !!!")
+console.warn("Warn Warn Warn")
+
+console.clear()
