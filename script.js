21cbc9d01b347f757eaab080f66d18840462c34c
FatmanaK <janalikbolsin@gmail.com>
2024-08-29 14:20:14 -0500

    degiskenler

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-alert(" Siteye hosgeldiniz!! ");
 
-console.log("hosgeldiniz !!!!") /* chrome da inspect edince console da yazi gormek icin  */
-console.log(123)
-console.error(" Hata olustu !!!")
-console.warn("Warn Warn Warn")
+//degiskenler
+ var password;
+ 
+ password= "Fatmana"
+ console.log(password)
 
-console.clear()
+ yas=25;
+ console.log(yas)
