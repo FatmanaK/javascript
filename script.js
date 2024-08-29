f00f7ba33afb137d4a280adb4eb104c1ac7e6d32
FatmanaK <janalikbolsin@gmail.com>
2024-08-29 16:46:40 -0500

    degisken turleri1

--- script.js
+++ script.js
@@ -7,3 +7,20 @@
 
  yas=25;
  console.log(yas)
+
+ //degisken turleri
+ //String
+ let firstName="Fatma";
+ console.log(typeof firstName)
+
+ //Number
+ let age=35;
+ console.log(typeof age)
+
+ //Boolean
+ let kredi_kullanimi=false;
+ console.log(typeof kredi_kullanimi)
+
+ //undefined
+ let phone;
+ console.log(typeof phone)
